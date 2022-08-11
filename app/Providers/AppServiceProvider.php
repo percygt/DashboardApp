b8a304f0cd251e36cb4d35864094a81ad203ea1d
percygt <percyg.timon@gmail.com>
2022-08-11 15:46:15 +0800

    /

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      URL::forceScheme('https');
+      if (env('APP_ENV') === 'production') {
+        URL::forceSchema('https');
+    }
+
+    // condition 2
+    if (env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
+    }
+
+    // condition 3
+    if (env('APP_FORCE_HTTPS', false)) {
+        URL::forceScheme('https');
+    }
 
     }
 }
