a523e5f1b575b6550ce86fcf3d5acba5dd487361
percygt <percyg.timon@gmail.com>
2022-08-11 15:42:01 +0800

    /

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      URL::forceScheme('https');
+      if (env('APP_ENV') === 'production') {
+        $this->app['request']->server->set('HTTPS','on'); // this line
+
+        URL::forceSchema('https');
+    }
     }
 }
